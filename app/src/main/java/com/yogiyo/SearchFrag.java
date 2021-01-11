92d2cb62a39b5b052fdf15d5dc466d03584e7b45
tekken5953@naver.com <wodud1011>
2021-01-11 23:30:06 +0900

    WIP

--- app/src/main/java/com/yogiyo/SearchFrag.java
+++ app/src/main/java/com/yogiyo/SearchFrag.java
@@ -5,10 +5,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.SearchView;
+
+import java.util.Objects;
 
 public class SearchFrag extends Fragment {
 
@@ -16,6 +21,10 @@ public class SearchFrag extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.search_frag,container,false);
+
+        SearchView searchView = v.findViewById(R.id.search_searchView);
+        searchView.setIconified(false);
+
         return v;
     }
 }
